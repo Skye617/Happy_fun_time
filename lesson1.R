3c45cb1554db5d540a2ccc15e4d664721c509fde
Liu Skye <go617@outlook.com>
2021-08-19 14:13:49 -0400

    Frequent problems in R

--- lesson1.R
+++ lesson1.R
@@ -54,8 +54,50 @@ library(car)
 car::scatterplot(mpg ~ disp, 
                  data = mtcars)
 library(ggplot2)
-ggplot(data = mtcars, 
+my_plot <- ggplot(data = mtcars, 
        mapping = aes(x = disp, y = mpg)) +
   geom_point() +
   geom_smooth(method = "lm")
+
+plotly::ggplotly(my_plot)
+
 install.packages('plotly')
+
+install.packages(c("car", "ggplot2"))
+theme_minimal()
+
+st_joe_land <- read.delim(file = "stJoeLand.csv", sep = ",")
+
+st_joe_land <- read.csv(file = "stJoeLand.csv")
+
+install.packages(c("readr", "data.table"))
+#1. NA造成的问题
+sample(x = c(1:1000, NA), 
+       size = 1001) |>
+#以下是Seth添加的solution
+  mean(na.rm = TRUE)
+
+#2. NA造成的问题
+cor_data <- data.frame(x = sample(c(rnorm(n = 100), NA), 10000, replace = TRUE), 
+                       y = sample(c(rnorm(n = 100), NA), 10000, replace = TRUE), 
+                       z = sample(c(rnorm(n = 100), NA), 10000, replace = TRUE))
+#以下是Seth添加的solution
+cor(cor_data, use="pairwise.complete.obs")
+
+
+#怎么read files? summary function是干嘛的？？？
+#summary在读title的时候会把它们当作character，所以这个时候需要change into factors
+#summary(land_value)
+#解决方案：summary(as.factor(land_value$school_district))
+
+
+#3. 问题
+factor(seq(from = 1, to = 10, by = .5)) |> 
+  as.numeric() |>
+  mean()
+
+#解决方案
+factor(seq(from = 1, to = 10, by = .5)) |> 
+  as.character() |>
+  as.numeric() |>
+  mean()
